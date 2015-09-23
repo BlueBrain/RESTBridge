7cf040caa6862a825dd126018988728bf275d26a
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-09-23 17:16:31 +0200

    Clean up RestBridge API: * Single factory method entry point * Unified argv parsing * RAII * RestBridge informs about URIs to publish/subscribe to in client code

--- restbridge/RestBridge.cpp
+++ restbridge/RestBridge.cpp
@@ -1,5 +1,6 @@
 /* Copyright (c) 2014-2015, Human Brain Project
  *                          Grigori Chevtchenko <grigori.chevtchenko@epfl.ch>
+ *                          Stefan.Eilemann@epfl.ch
  *
  * This file is part of RESTBridge <https://github.com/BlueBrain/RESTBridge>
  *
@@ -33,129 +34,96 @@
 
 namespace restbridge
 {
-
-static const std::string PUBLISHER_SCHEMA_SUFFIX = "cmd://";
-static const std::string SUBSCRIBER_SCHEMA_SUFFIX = "resp://";
+namespace
+{
+static const std::string DEFAULT_SCHEME = "hbp";
+static const uint16_t DEFAULT_PORT = 4020;
+static const std::string PUBLISHER_SCHEME_SUFFIX = "req";
+static const std::string SUBSCRIBER_SCHEME_SUFFIX = "rep";
+}
 
 namespace detail
 {
 class RestBridge
 {
 public:
-    RestBridge( const std::string& hostname, const uint16_t port )
-        : serverRunning_( false )
-        , hostname_( hostname )
-        , port_( port )
-    {}
-
-    RestBridge( const int argc, const char** argv )
-        : serverRunning_( false )
+    RestBridge( const int argc, char* argv[] )
     {
         for( int i = 0; i < argc; ++i  )
         {
-            if( std::string( argv[i] ) == "--restbridge-zeq" )
+            if( std::string( argv[i] ) == "--rest" &&
+                i < argc && argv[i+1] && argv[i+1][0] != '-' )
             {
-                if( i == argc )
-                    RBTHROW( std::runtime_error( "Missing ZeroEQ schema" ));
-
-                try
-                {
-                     const servus::URI uri( std::string( argv[i+1] ));
-                     if( uri.getScheme().empty() )
-                         RBTHROW( std::runtime_error( "Empty ZeroEQ schema" ));
-
-                     if( uri.getHost().empty() )
-                         RBTHROW( std::runtime_error( "Empty host" ));
-
-                     if( !uri.getPort())
-                         RBTHROW( std::runtime_error( "Port is zero" ));
-
-                     schema_ = uri.getScheme();
-                     hostname_ = uri.getHost();
-                     port_ = uri.getPort();
-                }
-                catch( ... )
-                {
-                    RBTHROW( std::runtime_error( "URI parse error" ));
-                }
+                uri = servus::URI( std::string( argv[i+1] ));
             }
         }
+
+        if( uri.getScheme().empty( ))
+            uri.setScheme( DEFAULT_SCHEME );
+        if( uri.getPort() == 0 )
+            uri.setPort( DEFAULT_PORT );
+        if( uri.getHost().empty( ))
+            uri.setHost( "localhost" );
+
+        // Request handler
+        const std::string pubScheme = uri.getScheme() + PUBLISHER_SCHEME_SUFFIX;
+        const std::string subScheme = uri.getScheme() +SUBSCRIBER_SCHEME_SUFFIX;
+        _handler.reset( new RequestHandler( pubScheme, subScheme ));
+
+        // http server
+        Server::options options( *_handler );
+        _server.reset( new Server( options.address( uri.getHost( )).
+                                   port( std::to_string( int( uri.getPort( )))).
+                                   reuse_address( true )));
+
+        RBDEBUG << "RestBridge running on http://" << uri.getHost() << ":"
+                << uri.getPort() << std::endl;
+
+        thread.reset( new boost::thread(
+                          boost::bind( &detail::Server::run, _server.get( ))));
     }
 
     ~RestBridge()
     {
-        if( serverRunning_ )
-        {
-            try
-            {
-                stop();
-            }
-            catch( ... ) {}
-        }
+        if( _server )
+            _server->stop();
+        if( thread )
+            thread->join();
+        RBDEBUG << "HTTP Server stopped" << std::endl;
     }
 
-    bool waitForRunningState(const uint16_t milliseconds)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-        return serverRunning_;
-    }
+    servus::URI uri;
+    std::unique_ptr< boost::thread > thread;
 
-    void run( const std::string& schema )
-    {
-        if( !schema.empty() )
-            schema_ = schema;
-        std::unique_lock<std::mutex> lock(mutex_);
-        const std::string publisherSchema = schema_ + PUBLISHER_SCHEMA_SUFFIX;
-        const std::string subscriberSchema = schema_ + SUBSCRIBER_SCHEMA_SUFFIX;
-        // Create request handler
-        RequestHandler handler( publisherSchema, subscriberSchema );
-
-        server::options options( handler );
-        std::stringstream port;
-        port << port_;
-        // Create http server
-        httpServer_.reset( new server( options.address( hostname_ ).port( port.str( ))));
-
-        RBDEBUG << "HTTP Server runnning on " << hostname_ << ":" << port_ << std::endl;
-        RBDEBUG << "ZeroEq Publisher Schema : " << publisherSchema << std::endl;
-        RBDEBUG << "ZeroEq Subscriber Schema: " << subscriberSchema << std::endl;
-        serverRunning_ = true;
-        // Start processing http requests
-        httpServer_->run();
-    }
-
-    void stop()
-    {
-        if( !thread_ || !serverRunning_ )
-            RBTHROW( std::runtime_error( "HTTP server is not running, cannot stop it" ));
-        // Stop http server hosted by thread_
-        httpServer_->stop();
-
-        // Wait until the http server is stopped
-        std::unique_lock<std::mutex> lock(mutex_);
-        // Stop listening thread
-        thread_->join();
-        serverRunning_ = false;
-        RBDEBUG << "HTTP Server on " << hostname_ << ":" << port_
-                << " is stopped" << std::endl;
-    }
-
-    boost::scoped_ptr< boost::thread > thread_;
-    boost::scoped_ptr< server > httpServer_;
-    std::mutex mutex_;
-    std::atomic<bool> serverRunning_;
-    std::string hostname_;
-    uint16_t port_;
-    std::string schema_;
+private:
+    std::unique_ptr< Server > _server;
+    std::unique_ptr< RequestHandler > _handler;
 };
 }
 
-RestBridge::RestBridge( const std::string& hostname, const uint16_t port )
-    : _impl( new detail::RestBridge( hostname, port ))
+std::unique_ptr< RestBridge > RestBridge::parse( const int argc, char* argv[] )
 {
+    for( int i = 0; i < argc; ++i  )
+        if( std::string( argv[i] ) == "--rest" )
+            return std::unique_ptr< RestBridge >( new RestBridge( argc, argv ));
+    return nullptr;
 }
 
-RestBridge::RestBridge( const int argc, const char** argv )
+std::unique_ptr< RestBridge > RestBridge::create( const int argc, char* argv[] )
+{
+    return std::unique_ptr< RestBridge >( new RestBridge( argc, argv ));
+}
+
+std::string RestBridge::getHelp()
+{
+    static const std::string help(
+        "  --rest [host][:port]: Enable the REST bridge. Optional parameters\n"
+        "         configure the web server, running by default on :" +
+                                  std::to_string( int( DEFAULT_PORT )));
+    return help;
+}
+
+RestBridge::RestBridge( const int argc, char* argv[] )
     : _impl( new detail::RestBridge( argc, argv ))
 {
 }
@@ -165,19 +133,21 @@ RestBridge::~RestBridge()
     delete _impl;
 }
 
-void RestBridge::run( const std::string& schema )
+servus::URI RestBridge::getPublisherURI() const
 {
-    if( _impl->thread_ )
-        RBTHROW( std::runtime_error( "HTTP server is already running" ));
-    _impl->thread_.reset( new boost::thread(
-        boost::bind( &detail::RestBridge::run, _impl, schema )));
-    if( !_impl->waitForRunningState(2000) )
-        RBTHROW( std::runtime_error( "HTTP server could not be started" ));
+    return servus::URI( _impl->uri.getScheme() + SUBSCRIBER_SCHEME_SUFFIX +
+                        "://" );
 }
 
-void RestBridge::stop()
+servus::URI RestBridge::getSubscriberURI() const
 {
-    _impl->stop();
+    return servus::URI( _impl->uri.getScheme() + PUBLISHER_SCHEME_SUFFIX +
+                        "://?subscribeSelf=true" );
+}
+
+bool RestBridge::isRunning() const
+{
+    return _impl->thread != nullptr;
 }
 
 }
--- tests/RestBridge.cpp
+++ tests/RestBridge.cpp
@@ -23,82 +23,38 @@
 #include <restbridge/RestBridge.h>
 #include <random>
 
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<unsigned long long> dis(0,60000);
+servus::Strings args = {{ "--rest" }, { "--chocolate" }, { "is-good" },
+                        { "--something-different..." }, { "is-not-good" },
+                        { "--(some-well-known-OS)8" },
+                        { "is-not-really-chocolate" }};
 
-BOOST_AUTO_TEST_CASE( test_construction )
+BOOST_AUTO_TEST_CASE( construction_argc_argv )
 {
-    BOOST_CHECK_NO_THROW( restbridge::RestBridge( "localhost", 12345 ));
-}
-
-BOOST_AUTO_TEST_CASE( test_construction_argc_argv )
-{
-    const char* argv[] = { "--chocolate", "is-good",
-                           "--something-different...", "is-not-good",
-                           "--restbridge-zeq", "zeq01://localhost:6666",
-                           "--(some-well-known-OS)8", "is-not-really-chocolate" };
+    std::string uri = "zeq01://localhost:6666";
+    char* argv[] = { &args[1][0], &args[2][0], &args[3][0], &args[4][0],
+                     &args[0][0], &uri[0], &args[4][0], &args[5][0] };
     const int argc = 8;
-    BOOST_CHECK_NO_THROW( restbridge::RestBridge( argc, argv ));
+    BOOST_CHECK_NO_THROW( restbridge::RestBridge::parse( argc, argv ));
 }
 
-BOOST_AUTO_TEST_CASE( test_construction_argc_argv_invalid_schema )
+BOOST_AUTO_TEST_CASE( construction_argc_argv_invalid_schema )
 {
-    const char* argv[] = { "--chocolate", "is-good",
-                           "--restbridge-zeq", "://localhost:6666",
-                           "--something-different...", "is-not-good",
-                           "--(some-well-known-OS)8", "is-not-really-chocolate" };
+    std::string uri = "foo://localhost:murks";
+    char* argv[] = { &args[1][0], &args[2][0], &args[3][0], &args[4][0],
+                     &args[0][0], &uri[0], &args[4][0], &args[5][0] };
     const int argc = 8;
-    BOOST_CHECK_THROW( restbridge::RestBridge( argc, argv ), std::runtime_error );
+    BOOST_CHECK_THROW( restbridge::RestBridge::parse( argc, argv ),
+                       std::exception );
 }
 
-BOOST_AUTO_TEST_CASE( test_construction_argc_argv_invalid_port )
+BOOST_AUTO_TEST_CASE( argc_argv_run )
 {
-    const char* argv[] = { "--(some-well-known-OS)8", "is ??",
-                            "--restbridge-zeq", "zeq01://localhost:" };
-    const int argc = 4;
-    BOOST_CHECK_THROW( restbridge::RestBridge( argc, argv ), std::runtime_error );
-}
-
-BOOST_AUTO_TEST_CASE( test_construction_argc_argv_invalid_hostname )
-{
-    const char* argv[] = { "--compleeeete", "#$%&",
-                            "--restbridge-zeq", "zeq01://:42" };
-    const int argc = 4;
-    BOOST_CHECK_THROW( restbridge::RestBridge( argc, argv ), std::runtime_error );
-}
-
-
-BOOST_AUTO_TEST_CASE( test_argc_argv_run )
-{
-    const char* argv[] = { "--chocolate", "is-good",
-                           "--something-different...", "is-not-good",
-                           "--(some-well-known-OS)8", "is-not-really-chocolate",
-                           "--restbridge-zeq", "zeq02://localhost:6666" };
+    std::string uri = "zeq02://localhost:6666";
+    char* argv[] = { &args[1][0], &args[2][0], &args[3][0], &args[4][0],
+                     &args[0][0], &uri[0], &args[4][0], &args[5][0] };
     const int argc = 8;
-    restbridge::RestBridge bridge( argc, argv );
-    BOOST_CHECK_NO_THROW( bridge.run());
-}
-
-BOOST_AUTO_TEST_CASE( test_invalid_run )
-{
-    const unsigned short port =  dis(gen) + 1024;
-    restbridge::RestBridge bridge( "localhost", port );
-    bridge.run( "bla" );
-    BOOST_CHECK_THROW( bridge.run( "blub" ), std::runtime_error );
-}
-
-BOOST_AUTO_TEST_CASE( test_invalid_stop )
-{
-    const unsigned short port =  dis(gen) + 1024;
-    restbridge::RestBridge bridge( "localhost", port );
-    BOOST_CHECK_THROW( bridge.stop(), std::runtime_error );
-}
-
-BOOST_AUTO_TEST_CASE( test_run_stop )
-{
-    const unsigned short port =  dis(gen) + 1024;
-    restbridge::RestBridge bridge( "localhost", port );
-    bridge.run( "bla" );
-    bridge.stop();
+    std::unique_ptr< restbridge::RestBridge > bridge =
+        restbridge::RestBridge::parse( argc, argv );
+    BOOST_CHECK( bridge );
+    BOOST_CHECK( bridge->isRunning( ));
 }
